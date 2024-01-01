d8dcfa96153ee7e52b796af58ef96121efe548ad
KK-Designs <KKart2007@gmail.com>
2023-12-31 23:16:52 -0800

    Return ResultObject instead of boolean and update typings

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,15 @@
-export default function Create(name: string, path: string, args: string[]): boolean | object;
-export default function Delete(name: string): boolean | object;
-export default function Enable(name: string): boolean | object;
-export default function Disable(name: string): boolean | object;
-export default function Status(name: string): object;
+interface ResultObject {
+	failed: Boolean,
+	error: null | Error
+}
+
+interface StatusObject {
+	exists: Boolean,
+	status: 'ENABLED' | 'DISABLED' | 'UNKNOWN' | null
+}
+
+export default function Create(name: string, path: string, args: string[]): ResultObject;
+export default function Delete(name: string): ResultObject;
+export default function Enable(name: string): ResultObject;
+export default function Disable(name: string): ResultObject;
+export default function Status(name: string): StatusObject;
